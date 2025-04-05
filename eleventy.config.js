eabe2e23703bb3884bbade8418a51a939a8f6a84
Matthew Litwin <mlitwin@sonic.net>
2025-04-04 21:58:19 -0700

    what did it do the the svgs?

--- eleventy.config.js
+++ eleventy.config.js
@@ -85,7 +85,7 @@ export default async function(eleventyConfig) {
 	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		// Output formats for each image.
-		formats: ["avif", "webp", "auto"],
+		formats: ["auto"],
 
 		// widths: ["auto"],
 
